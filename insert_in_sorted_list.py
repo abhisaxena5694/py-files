2980863358c6fa0c90a9447d209cb9ff3496d9a3
abhisaxena5694 <abhisaxena5694@gmail.com>
2016-02-20 20:01:08 +0530

    Update insert_in_sorted_list.py

--- insert_in_sorted_list.py
+++ insert_in_sorted_list.py
@@ -3,17 +3,17 @@ It then returns a new sorted list with the number inserted in its appropriate po
 def insert_in_sorted_list():
     DATA_LIST = input("Enter a sorted list of numbers:")
     element = input("Enter the number to be inserted:")
-    if type(DATA_LIST) != list:
+    if type(DATA_LIST) != list: 
         DATA_LIST = list(DATA_LIST)
     list_length = len(DATA_LIST)
-    for index in range(list_length):
+    for index in range(list_length): # finds the position where the new element has to be inserted
         if element > DATA_LIST[index]:
             position = index + 1
         #else: 
         #    continue
     count = list_length
     DATA_LIST.append(None)
-    while count >= position:
+    while count >= position: # creates space in the list for inserting the new element 
         DATA_LIST[count] = DATA_LIST[count - 1]
         count -= 1
     DATA_LIST[position] = element

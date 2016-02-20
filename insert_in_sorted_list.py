34dfc4c0d98f946a066ac6ab5750c36f157c8b01
abhisaxena5694 <abhisaxena5694@gmail.com>
2016-02-20 17:41:12 +0530

    Update insert_in_sorted_list.py

--- insert_in_sorted_list.py
+++ insert_in_sorted_list.py
@@ -1,4 +1,5 @@
-"""This function asks the user for a sorted list of numbers and a number to be inserted in the list. It then returns a new sorted list with the number inserted in its appropriate position."""
+"""This function asks the user for a sorted list of numbers and a number to be inserted in the list. 
+It then returns a new sorted list with the number inserted in its appropriate position."""
 def insert_in_sorted_list():
     DATA_LIST = input("Enter a sorted list of numbers:")
     element = input("Enter the number to be inserted:")

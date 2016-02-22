55303db5e3279d8eb75c705b94498eea460c9099
abhisaxena5694 <abhisaxena5694@gmail.com>
2016-02-22 15:18:58 +0530

    Update linear-search.py

--- linear-search.py
+++ linear-search.py
@@ -1,8 +1,9 @@
+#this function performs a linear search on a list of numbers to search for an element.
 def linear_search():
-    DATA_LIST = input("Enter a list of numbers")
-    element = input("Enter the element to be searched")
+    DATA_LIST = input("Enter a list of numbers:")
+    element = input("Enter the element to be searched:")
     indicator = 0
-    for i in DATA_LIST:
+    for i in range(len(DATA_LIST)):
         if DATA_LIST[i] == element:
             print "Element found at", i, "index position."
             indicator = 1

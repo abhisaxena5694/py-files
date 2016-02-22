2f52538f08cbf48342722a30c558ff587a9fcc95
abhisaxena5694 <abhisaxena5694@gmail.com>
2016-02-22 15:40:31 +0530

    Update linear-search.py

--- linear-search.py
+++ linear-search.py
@@ -5,7 +5,7 @@ def linear_search():
     flag = 0 # flag indicates whether the 'element' has been found or not; 0 = "NOT FOUND", 1 = "FOUND"
     for i in range(len(DATA_LIST)): #loop for traversing the list to compare 'element' with all the list members.
         if DATA_LIST[i] == element:
-            print "Element found at", i + 1, " position."
+            print "Element found at", i + 1, "position."
             flag = 1
             break
     if flag == 0:
